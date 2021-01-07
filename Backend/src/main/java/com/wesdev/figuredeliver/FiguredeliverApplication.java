ee5b6379f111fa0c1d40162f681d59b8d7006be0
wes <wesllen.v.costa@mail.com>
2021-01-07 13:18:54 -0400

    Start

---
+++ Backend/src/main/java/com/wesdev/figuredeliver/FiguredeliverApplication.java
@@ -0,0 +1,13 @@
+package com.wesdev.figuredeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiguredeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiguredeliverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/wesdev/figuredeliver/FiguredeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesdev.figuredeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiguredeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
